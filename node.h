2de37b75611cdbfd5dd18d23f5b54a1c8e988004
Gaurav Singh <71246220+gaurav-singh-2003@users.noreply.github.com>
2021-04-24 18:58:25 +0530

    Update node.h

--- node.h
+++ node.h
@@ -5,25 +5,22 @@
     The comparator function takes as input the pointers of two nodes and outputs whether the first is 
     lesser than the other. You can use this comparator for deciding the order of 2 nodes in the priority 
     queue.
-
     This file will be what the user will edit, while trying to use your library. Once you complete writing
     the library, you have to (as an user of the library), write the comparators for DFS, BFS, greedy 
     search (A-star lite).
-
     Check out the input file ts_data_1.txt. It has input of format 
     <number_of_nodes>:n
     <state_number> <value> <parent_state_number>
     ...
     n times
-
     For now, write the main function and input the nodes, fill the values of number_of_children, children
     (in the struct) and try to write the priority queue using the comparator.
 */
 
 #include <stdio.h>
 #include <stdbool.h>
-typedef struct node node;
-struct node
+
+typedef struct node
 {
     int state_number;
     int value;
@@ -31,8 +28,8 @@ struct node
 
     // You (as a developer) can add any extra information here [Will not be touched by user]
     int depth, seen_time, number_of_children;
-    node *children[10000];
-};
+    struct node *children[10000];
+}node;
 
 bool node_comparator(node *a, node *b)
 {
