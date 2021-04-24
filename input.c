5c9eaf3c62eaf0eebde500ccae1b200fb104cb49
ankithvarun <ankith.varun@research.iiit.ac.in>
2021-04-24 18:44:24 +0530

    removed main() and added comments

--- input.c
+++ input.c
@@ -5,9 +5,9 @@
 
 typedef node *Node;
 
-void generateTree(Node *NodeArray, int N)
+void generateTree(Node *NodeArray, int N)   //generates a tree out of the array of nodes
 {
-    for (int i = 1; i < N; i++)
+    for (int i = 1; i < N; i++)     //traverses through each node (other than root) and links it to its parent
     {
         Node Temp = NodeArray[NodeArray[i]->parent - 1];
         Temp->children[Temp->number_of_children] = NodeArray[i];
@@ -16,14 +16,14 @@ void generateTree(Node *NodeArray, int N)
     }
 }
 
-Node inputTree()
+Node inputTree()    //takes input from the user and returns a tree
 {
     int NoOfNodes;
     scanf("%d", &NoOfNodes);
 
     Node *NodeArray = (Node *)malloc(NoOfNodes * sizeof(node));
 
-    for (int i = 0; i < NoOfNodes; i++)
+    for (int i = 0; i < NoOfNodes; i++)     //takes each node as input and feeds it into an array of nodes
     {
         NodeArray[i] = (Node)malloc(sizeof(node));
         assert(NodeArray != NULL);
@@ -32,13 +32,13 @@ Node inputTree()
         NodeArray[i]->seen_time = 0;
     }
 
-    Node Root = NodeArray[0];
+    Node Root = NodeArray[0];   //initialises the first node as root of the tree
     Root->depth = 0;
-    generateTree(NodeArray, NoOfNodes);
+    generateTree(NodeArray, NoOfNodes);     //generates the required tree
     return Root;
 }
 
-void printNode(Node TreeNode)
+void printNode(Node TreeNode)       //prints the attributes of a node
 {
     printf("State Number: %d\n", TreeNode->state_number);
     printf("Value: %d\n", TreeNode->value);
@@ -62,16 +62,10 @@ void printNode(Node TreeNode)
     printf("\n\n");
 }
 
-void printTree(Node TreeNode)
+void printTree(Node TreeNode)   //traverses through each node of the tree and prints its attributes
 {
     printNode(TreeNode);
 
     for (int i = 0; i < TreeNode->number_of_children; i++)
         printTree(TreeNode->children[i]);
-}
-
-int main()
-{
-    Node Root = inputTree();
-    printTree(Root);
 }
